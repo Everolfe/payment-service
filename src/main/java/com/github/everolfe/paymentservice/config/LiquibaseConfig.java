24018290cfab71cd28c369bd1dde174f588fc106
Everolfe <rusakgeorgy@gmail.com>
2026-04-24 21:07:25 +0300

    release: db-structure

---
+++ src/main/java/com/github/everolfe/paymentservice/config/LiquibaseConfig.java
@@ -0,0 +1,41 @@
+package com.github.everolfe.paymentservice.config;
+
+import liquibase.command.CommandScope;
+import liquibase.command.CommandResults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LiquibaseConfig {
+
+    @Value("${spring.liquibase.change-log}")
+    private String changeLog;
+
+    @Value("${ROOT_USERNAME}")
+    private String username;
+
+    @Value("${ROOT_PASSWORD}")
+    private String password;
+
+    @Value("${DB_NAME}")
+    private String database;
+
+    @Bean
+    @ConditionalOnProperty(name = "spring.liquibase.enabled", havingValue = "true", matchIfMissing = false)
+    public CommandResults runLiquibase() {
+        try {
+            return new CommandScope("update")
+                    .addArgumentValue("changelogFile", changeLog)
+                    .addArgumentValue(
+                            "url",
+                            "mongodb://" + username + ":" + password +
+                                    "@mongo-db:27017/" + database + "?authSource=admin"
+                    )
+                    .execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
